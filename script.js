b2c876ac24b1a3f0e9c3f7b82f7159aa1fa4043e
roxanneferentz <roxanneferentz@students.berkeley.net>
2019-03-20 14:25:54 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function addShirt(){
+    //documents user input and adds new row for additional input
+}
+function addPant(){
+    //documents user input and adds new row for additional input
+}
+function addOther(){
+    //documents user input and adds new row for additional input
+}
